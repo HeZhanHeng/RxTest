97ddb08a34f9f80193e76fd6b0da232e1b5cf60b
zhen22 <zhanhenghe@zhen22.com>
2019-11-27 18:43:19 +0800

    RxJava分步骤以及基于事件流的链式两种实现方式。

--- app/src/main/java/com/example/zhen22/rxtest/MainActivity.java
+++ app/src/main/java/com/example/zhen22/rxtest/MainActivity.java
@@ -17,7 +17,11 @@ private static final String TAG="RxJava";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        创建被观察者&生产事件
+        initFirst();//方式一：分步骤实现
+        initSecond();//方式二：基于事件流的链式调用
+    }
+    private void initFirst(){
+        //        创建被观察者&生产事件
         io.reactivex.Observable<Integer> observable= io.reactivex.Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> e) throws Exception {
@@ -50,7 +54,39 @@ private static final String TAG="RxJava";
                 Log.d(TAG, "对Complete事件作出响应");
             }
         };
-//        通过订阅连接被观察者和观察者
+//        通过订阅连接被观察者和观察者(点菜)
         observable.subscribe(observer);
+    }
+    private void initSecond(){
+        io.reactivex.Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onNext(4);
+                e.onNext(5);
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用subscribe2连接");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "对Next事件"+value+"做出响应");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件做出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        });
     }
 }
