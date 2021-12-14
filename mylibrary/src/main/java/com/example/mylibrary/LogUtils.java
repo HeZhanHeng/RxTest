04e99587d9eb7623ce8041c19b3d562bb498b6c8
HeZhanHeng <1104791552@qq.com>
2021-12-14 23:32:22 +0800

    添加library

---
+++ mylibrary/src/main/java/com/example/mylibrary/LogUtils.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+public class LogUtils {
+    private static final String TAG ="zhen22++++";
+    public static void show(String msg){
+        Log.d(TAG, msg);
+    }
+}
